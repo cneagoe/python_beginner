096362911de8364acc8c077e68de0dba36403dab
Ciprian Neagoe <ng.ciprian@gmail.com>
2023-02-26 23:03:39 +0200

    finished 6

--- python_basics_6.py
+++ python_basics_6.py
@@ -132,3 +132,98 @@ def count_matching_elements(my_matrix, element):
 my_matrix = [[1, 2, 5], [4, 5, 6], [7, 8, 9]]
 element = 5
 print(count_matching_elements(my_matrix, element))
+
+# CONCEPT : min max functions
+# EXPLANATION : min and max are built in functions that can calculate the min and max of a list
+l = [2, 3, 6, 1, 0]
+print(min(l))
+print(max(l))
+
+# EXERCISE 7 :
+# Write a Python program to remove sublists from a given list of lists 
+# that contain an element outside a given range.
+def remove_list_range(input_list, left_range, rigth_range) :
+    n = []
+    for i in input_list :
+        if min(i)>=left_range and max(i)<=rigth_range :
+            n.append(i)
+    return n
+list1 = [[2], [0], [1, 2, 3], [0, 1, 2, 3, 6, 7], [9, 11], [13, 14, 15, 17]]
+left_range = 13
+rigth_range = 17
+print("Original list:")
+print(list1)
+print("\nAfter removing sublists from a given list of lists, which contains an element outside the given range:")
+print(remove_list_range(list1, left_range, rigth_range))
+
+# EXERCISE 8 :
+# Write a Python program to rotate each list from a nested list 
+# by a specified number of items in the right or left direction.
+def rotate_list(my_list, direction, rotation):
+    nl = []
+    if direction == "right":
+        rotation = -rotation
+    for l in my_list :
+        nl.append(l[rotation:] + l[:rotation])
+    return nl
+my_list = [[1, 2, 7, 2, 9, 7], [3, 4, 1, 0, 6], [5, 4, 4, 8, 7, 3]]
+rotated_list = rotate_list(my_list, "right", 2)
+print(rotated_list)
+rotated_list = rotate_list(my_list, "left", 2)
+print(rotated_list)
+
+# EXERCISE 9 :
+# Write a Python program to find nested list elements that are present in another list
+def find_intersection(my_list, nested_lists):
+    big_list = []
+    for nested_list in nested_lists:
+        intersection = []
+        for num in nested_list :
+            if num in my_list :
+                intersection.append(num)
+        big_list.append(intersection)
+    return big_list
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+nested_lists = [[12, 18, 23, 25, 45], [7, 11, 19, 24, 28], [1, 5, 8, 18, 15, 16]]
+intersections = find_intersection(my_list, nested_lists)
+print(intersections)
+
+# EXERCISE 10 :
+# Write a Python program to interleave multiple lists of the same length.
+def interleave_multiple_lists(list1,list2):
+    result = []
+    for a in range(0, len(list1)):
+        result.append(list1[a])
+        result.append(list2[a])
+    return result
+list1 = [1,2,3,4,5,6,7]
+list2 = [10,20,30,40,50,60,70]
+print(interleave_multiple_lists(list1,list2))
+
+# EXERCISE 11 :
+# Write a Python program to calculate the sum of the numbers in a list between the indices of a specified range.
+def sum_Range_list(nums, m, n):                                                                                                                                                                                                
+    sum_range = 0                                                                                                                                                                                                         
+    for i in range(m, n+1, 1):                                                                                                                                                                                        
+        sum_range += nums[i]                                                                                                                                                                                                  
+    return sum_range   
+nums = [2,1,5,6,8,3,4,9,10,11,8,12]
+m = 8
+n = 10
+print(sum_Range_list(nums, m, n))
+
+# EXERCISE 12 :
+# Write a Python program to remove words from a given list of strings containing a character or string
+def remove_words(in_list, char_list):
+    new_list = []
+    for line in in_list:
+        clean_word = ""
+        for l in line :
+            if l not in char_list :
+                clean_word += l
+        new_list.append(clean_word)
+    return new_list
+str_list = ['Red color', 'Orange#', 'Green', 'Orange @', "White"]
+char_list = ['#', 'color', '@']
+print(char_list)
+print(remove_words(str_list, char_list))
